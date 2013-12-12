d1bd8a5598ab4bfbf376268b2db74ab1dbcbf501
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2013-12-12 16:32:30 +0100

    Updating nginx recipe & startup scripts

--- recipes/install/ruby.sh
+++ recipes/install/ruby.sh
@@ -1,22 +1,37 @@
-aptitude install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison git pwgen vim libcurl4-openssl-dev
+# [TODO] Think about permissions especially to /srv chown it to www ?.
+# [TODO] Reorganize it, so the ruby installation is a separate step, then nginx is another, then db
+
+aptitude update
+
+aptitude install build-essential openssl libreadline6 libreadline6-dev curl \
+  git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 \
+  libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool \
+  bison git pwgen vim libcurl4-openssl-dev
 
 chmod 777 /opt
 
 adduser www
 su www
+
 cd ~
 
 curl -L get.rvm.io | bash -s stable
 
-echo >> .bashrc
-echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*' >> .bashrc
+# echo >> .bashrc
+# echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*' >> .bashrc
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 rvm install 1.9.3
 rvm 1.9.3
 
 gem install passenger bundler
 
-passenger-install-nginx-module 
+passenger-install-nginx-module
+mkdir /var/log/nginx
+
+mkdir -p /srv/default
+touch /srv/default/index.html
 
 exit # Back to root
 
